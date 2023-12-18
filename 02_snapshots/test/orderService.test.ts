061c4fe2cdb53bedb7c8bd0d4485881b875d3f83
balazsvalyon <balazs.valyon@warnermedia.com>
2023-12-18 22:10:43 +0100

    Remove unneeded package references

--- 02_snapshots/test/orderService.test.ts
+++ 02_snapshots/test/orderService.test.ts
@@ -6,27 +6,33 @@ import { mock, mockReset } from 'jest-mock-extended';
 const mockedUserService = mock<UserService>();
 
 describe('OrderService tests', () => {
+    let sut: OrderService;
+
+    const dummyUser = new User(
+        1,
+        'john_doe',
+        'john@example.com',
+        'John',
+        'Doe',
+        new Date('1990-01-15'),
+        {
+            street: '123 Main St',
+            city: 'Anytown',
+            zipCode: '12345',
+        },
+        '123-456-7890',
+        true,
+        ['user', 'admin']
+    );
+
+    beforeEach(() => {
+        mockReset(mockedUserService);
+        sut = new OrderService(mockedUserService);
+    })
 
     it('should return all the users who made an order in the last hour', () => {
         // Arrange
-        const dummyUser = new User(
-            1,
-            'john_doe',
-            'john@example.com',
-            'John',
-            'Doe',
-            new Date('1990-01-15'),
-            {
-                street: '123 Main St',
-                city: 'Anytown',
-                zipCode: '12345',
-            },
-            '123-456-7890',
-            true,
-            ['user', 'admin']
-        );
         mockedUserService.getUserById.mockReturnValue(dummyUser);
-        const sut = new OrderService(mockedUserService);
 
         // Act
         const actualResult = sut.getUsersOfTheLastHour();
@@ -34,5 +40,14 @@ describe('OrderService tests', () => {
         // Assert
         expect(actualResult).toMatchSnapshot();
         expect(mockedUserService.getUserById).toMatchSnapshot();
+    })
+
+    it('should throw an error when tries to get all the users', () => {
+        // Arrange
+        const error = new Error('something bad happened');
+        mockedUserService.getUserById.mockImplementation(() => { throw error });
+
+        // Act and assert
+        expect(() => sut.getUsersOfTheLastHour()).toMatchInlineSnapshot(`[Function]`);
     })
 })
