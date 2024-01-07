758e79f40d7f374fe5cef8ecc0c46d85e9883358
tomi <hering.tamas70@gmail.com>
2024-01-07 09:36:46 +0100

    add methods

--- 00_empty_project/src/calculator.ts
+++ 00_empty_project/src/calculator.ts
@@ -3,5 +3,27 @@ export class Calculator {
     public Add(a: number, b: number): number {
         return a + b;
     }
+    public Subtract(a: number, b: number): number {
+        return a - b;
+    }
+    public Multiply(a: number, b: number): number {
+        return a * b;
+    }
+    public Divide(a: number, b: number): number {
+        if (b === 0) {
+            throw new Error('Cannot divide by zero');
+        }
+        return a / b;
+    }
+    public SquareRoot(a: number): number {
+        if (a < 0) {
+            throw new Error('Cannot square root of a negative number');
+        }
+        return Math.sqrt(a);
+    }
+    public Power(a: number, b:number): number {
+        return Math.pow(a, b);
+    }
+
 
 }
